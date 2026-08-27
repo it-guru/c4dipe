3ecda905976876c3902cbb54258466f3a6447098
Hartmut Vogler <it@guru.de>
2026-08-27 15:52:54 +0200

    .

--- lib/kernel/dataobj/__init__.py
+++ lib/kernel/dataobj/__init__.py
@@ -2,6 +2,8 @@ from .base import DataObj
 
 from .SQLDB   import DataObjSQLDB
 from .Static  import DataObjStatic
+from .Rest    import DataObjRest
+from .Elastic import DataObjElastic
 
 __all__ = [k for k in locals() if k.startswith("DataObj")]
 
